009395ff4f3c97eab4e4d223fa10b02cab425608
Samuel Antao <sfantao@us.ibm.com>
2016-02-18 00:24:02 -0500

    Implement code generation for target exit data.

--- lib/CodeGen/CGOpenMPRuntime.cpp
+++ lib/CodeGen/CGOpenMPRuntime.cpp
@@ -4891,12 +4891,14 @@ void CGOpenMPRuntime::emitTargetDataCalls(CodeGenFunction &CGF,
   }
 }
 
-void CGOpenMPRuntime::emitTargetEnterDataCall(CodeGenFunction &CGF,
+void CGOpenMPRuntime::emitTargetEnterOrExitDataCall(CodeGenFunction &CGF,
                                               const OMPExecutableDirective &D,
                                               const Expr *IfCond,
                                               const Expr *Device) {
   if (!CGF.HaveInsertPoint())
     return;
+
+  assert((isa<OMPTargetEnterDataDirective>(D) || isa<OMPTargetExitDataDirective>(D)) && "Expecting either target enter or exit data directives.");
 
   // Generate the code for the opening of the data environment.
   auto &&ThenGen = [this, &D, &CGF, Device](CodeGenFunction &CGF) {
@@ -4938,7 +4940,7 @@ void CGOpenMPRuntime::emitTargetEnterDataCall(CodeGenFunction &CGF,
     llvm::Value *OffloadingArgs[] = {
         DeviceID,         PointerNum,    BasePointersArrayArg,
         PointersArrayArg, SizesArrayArg, MapTypesArrayArg};
-    CGF.EmitRuntimeCall(createRuntimeFunction(OMPRTL__tgt_target_data_begin),
+    CGF.EmitRuntimeCall(createRuntimeFunction(isa<OMPTargetEnterDataDirective>(D) ? OMPRTL__tgt_target_data_begin : OMPRTL__tgt_target_data_end),
                         OffloadingArgs);
   };
 
