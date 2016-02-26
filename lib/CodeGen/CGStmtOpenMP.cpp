ca48420a16800baf65cf33bc15d991c61934ae3a
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2016-02-26 12:35:16 +0000

    Add OMPD_simd to DKind test.

--- lib/CodeGen/CGStmtOpenMP.cpp
+++ lib/CodeGen/CGStmtOpenMP.cpp
@@ -1270,7 +1270,8 @@ CodeGenFunction::EmitOMPDirectiveWithLoop(OpenMPDirectiveKind DKind,
   bool HasSimd = DKind == OMPD_parallel_for_simd || DKind == OMPD_for_simd ||
                  DKind == OMPD_distribute_simd ||
                  DKind == OMPD_teams_distribute_simd ||
-                 DKind == OMPD_target_teams_distribute_simd;
+                 DKind == OMPD_target_teams_distribute_simd ||
+                 DKind == OMPD_simd;
   printf("======> Inside EmitOMPDirectiveWithLoop (HasSimd: %d )\n", HasSimd);
   CGPragmaOmpSimd SimdWrapper(&S);
   llvm::Function *BodyFunction = 0;
