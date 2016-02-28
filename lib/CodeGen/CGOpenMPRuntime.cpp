e5286b3a848976cb95dc6b1f3161961c981cee13
Gheorghe-Teodor Bercea <gbercea@saffron.arc.ox.ac.uk>
2016-02-28 17:02:44 +0000

    Correctly detect SIMD pragma.

--- lib/CodeGen/CGOpenMPRuntime.cpp
+++ lib/CodeGen/CGOpenMPRuntime.cpp
@@ -2900,7 +2900,7 @@ class CGOpenMPRuntime_NVPTX: public CGOpenMPRuntime {
   // Look for a SIMD directive in the given code block
   bool BlockHasSimd(const Stmt &S) {
     printf("====> BlockHasSimd: isa<OMPSimdDirective>(S) %d\n",isa<OMPSimdDirective>(S));
-    if (!*S)
+    if (!&S)
       return false;
     if (isa<OMPSimdDirective>(S))
       return true;
